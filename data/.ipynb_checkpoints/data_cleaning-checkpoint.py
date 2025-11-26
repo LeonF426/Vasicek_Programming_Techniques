cf75b26997f10e69c093cfbca5aab2893d04b545
Leon Fiethen <l65599@aln.iseg.ulisboa.pt>
2025-11-26 23:26:26 +0000

    data added

--- data/.ipynb_checkpoints/data_cleaning-checkpoint.py
+++ data/.ipynb_checkpoints/data_cleaning-checkpoint.py
@@ -3,6 +3,40 @@
 # We import pandas, a package that specializes on efficient and convenient implementations of data structures
 import pandas as pd
 
+def DataCleanup(csvname: str = "DFF") -> None:
+    df = pd.read_csv(filepath_or_buffer=f"{csvname}.csv").dropna()
+
+    date_col = df.columns[0]
+    rates_col = df.columns[1]
+
+    #convert from string to datetime object
+    df[date_col] = pd.to_datetime(df[date_col])
+
+    # Extract min and max date
+    min_date , max_date = df[date_col].min(), df[date_col].max()
+
+    # Create business day DatetimeIndex object to reindex existing data:
+    b_days = pd.bdate_range(start=min_date, end=max_date)
+
+    # reindex
+    df.set_index(keys=date_col,inplace=True)
+    df = df.reindex(b_days, method="ffill")
+    df.index.name = "date"
+
+    # to convert them to their "proper" values:
+    df[rates_col] /= 100.0
+
+    #Round to 4 decimal places
+    df[rates_col] = df[rates_col].round(4)
+
+    # Rename DGS10 column for convenience
+    df.rename(columns={"DGS10": "rate_pct"}, inplace=True)
+
+    # Save as cleaned dataset csv format:
+    df.to_csv(path_or_buf=f"{csvname.lower()}_clean.csv")
+    return None
+
+    
 df = pd.read_csv(filepath_or_buffer="DGS10.csv").dropna()
 
 # Take a look at the columns and if the data has been read in properly:
